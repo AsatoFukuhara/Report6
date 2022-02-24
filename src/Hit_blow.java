5b68e80cbc539797e214ecd5a042e7c7b5f3d51b
Asato Fukuhara <e205730@ie.u-ryukyu.ac.jp>
2022-02-25 07:00:23 +0900

    2nd commit

--- src/Hit_blow.java
+++ src/Hit_blow.java
@@ -4,51 +4,37 @@ import java.util.ArrayList;
 
 
 public class Hit_blow{
-    private int a;
-    private int b;
-    private int c;
-    private int d;
+    final static int answer_size = 3;
+    public int a, b, c;
+
+
     public int getA(){
         return this.a;
     }
     public int getB(){
         return this.b;
     }
-    public static void main(String[] args){
-        ArrayList<Integer> list = new ArrayList<Integer>();
-        //0~9の整数値を持つリストを用意
-        for(int i = 0 ; i < 10 ; i++) {
-            list.add(i);
+
+    public int getC(){
+        return this.c;
+    }
+    
+    public static int[] makeRightAnswer() {
+        int[] answer = new int[answer_size];
+        for(int i = 0; i < answer.length; i++) {
+        answer[i] = (int)(Math.random()*10);
+        for(int j = i-1; j >=0; j--) {
+        if(answer[j] == answer[i]) {
+        answer[i] = (int)(Math.random()*10);
         }
-        //リストを表示（確認用）
-        //System.out.println(list);
-
-        //shuffleメソッドで上で作ったリストをシャッフル
-        Collections.shuffle(list);
-        for(int i = 1; i < 5 ; i++){
-            //System.out.println(list.get(i));//このリストの最初から４番目までを表示したい=>完了
-            if (i == 1){
-                int a=list.get(i);
-                System.out.println("aは"+a);
-                }
-                else if (i == 2){
-                    int b = list.get(i);
-                    System.out.println("bは"+b);
-                }
-                else if (i == 3){
-                    int c = list.get(i);
-                    System.out.println("cは"+c);
-                }
-                else if (i == 4){
-                    int d = list.get(i);
-                    System.out.println("dは"+d); 
-                }
- 
-        //ランダムにした結果を表示
-        //System.out.println(list)
-    }
-    System.out.println(getA());
+        }
+        }
+        return answer;
+        }
 
 
-    }
-} 
+            public static void main(String[] args){
+                System.out.println();
+            }
+
+        }
